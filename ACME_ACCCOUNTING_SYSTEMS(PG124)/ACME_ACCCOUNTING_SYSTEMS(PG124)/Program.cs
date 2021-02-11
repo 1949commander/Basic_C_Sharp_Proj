e3666e6341e3c81cc306d8be11270acddcbc481f
Brian Reeves <1949commander@gmail.com>
2021-02-11 01:20:59 -0800

    Upload Project PG 124
    First couple exercises plus assignment pg 124

---
+++ ACME_ACCCOUNTING_SYSTEMS(PG124)/ACME_ACCCOUNTING_SYSTEMS(PG124)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACME_ACCCOUNTING_SYSTEMS_PG124_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
